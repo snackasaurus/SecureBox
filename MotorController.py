f1e185f3855d8d35694a1e9dae9ffcc42a48a765
Moe Sial <sial.moe@gmail.com>
2016-05-15 17:03:08 -0700

    added motor test

--- MotorController.py
+++ MotorController.py
@@ -8,6 +8,39 @@ class MotorController:
         mh = Adafruit_MotorHAT(addr=0x60)
         self.motor = mh.getMotor(1)
         self.motor.setSpeed(150)
+    
+    
+    def motor_test(self):
+        while (True):
+            print "Forward! "
+            self.motor.run(Adafruit_MotorHAT.FORWARD)
+         
+            print "\tSpeed up..."
+            for i in range(255):
+                self.motor.setSpeed(i)
+                time.sleep(0.01)
+         
+            print "\tSlow down..."
+            for i in reversed(range(255)):
+                self.motor.setSpeed(i)
+                time.sleep(0.01)
+         
+            print "Backward! "
+            self.motor.run(Adafruit_MotorHAT.BACKWARD)
+         
+            print "\tSpeed up..."
+            for i in range(255):
+                self.motor.setSpeed(i)
+                time.sleep(0.01)
+         
+            print "\tSlow down..."
+            for i in reversed(range(255)):
+                self.motor.setSpeed(i)
+                time.sleep(0.01)
+         
+            print "Release"
+            self.motor.run(Adafruit_MotorHAT.RELEASE)
+            time.sleep(1.0)
 
 def turnOffMotors():
     mh = Adafruit_MotorHAT(addr=0x60)
