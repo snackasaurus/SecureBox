8f0d3cd3588c1fcacc4f272be4f60ea449831acc
Moe Sial <sial.moe@gmail.com>
2016-05-28 11:22:44 -0700

    run built

--- keyPad.py
+++
@@ -1,7 +0,0 @@
-
-class KeyPad:
-    def __init__(self, message_queue, secret_code):
-        self.message_queue = message_queue
-
-    def wait_for_secret_code_match(self, secret_code):
-        print('Waiting for secret code')
